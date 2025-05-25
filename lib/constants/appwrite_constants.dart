1c4bf2b9fa5689354b066bb0f84f57e88c7ae97d
Sandeep <sandeepdewangan7@gmail.com>
2025-05-25 08:38:13 +0530

    Constants, Svgs, Theme Setup

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "683285d10034ffa877ef";
+  static const String projectId = "6832839600241ba19c2c";
+  static const String endPoint = "https://fra.cloud.appwrite.io/v1";
+}
